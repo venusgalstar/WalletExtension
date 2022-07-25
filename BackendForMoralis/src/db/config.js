a9f64352039bad40cc3c5d3be49901feb43afaf8
EugineePizerbert107 <EuginePizerbert107@gmail.com>
2022-07-26 04:01:16 +0900

    update backend.

--- BackendForMoralis/src/db/config.js
+++ BackendForMoralis/src/db/config.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 module.exports = {
-    url: "mongodb://localhost:27017/iglooWallet"
+    url: process.env.MONGODB_URI || "mongodb://localhost:27017/iglooWallet"
     // url: "mongodb+srv://root:rootroot@cluster0.okeov.mongodb.net/pink?retryWrites=true&w=majority"
     // url: "https://data.mongodb-api.com/app/data-rfucc/endpoint/data/v1"
 }
